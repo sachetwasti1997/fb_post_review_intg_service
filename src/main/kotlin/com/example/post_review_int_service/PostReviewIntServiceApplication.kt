3ae9ae4e84f00e40e54a6ef007743c84b1ec06b4
sachetwasti1997 <sachetwasti61@gmail.com>
2022-01-16 11:12:07 +0530

    [ADD]The project setup

---
+++ src/main/kotlin/com/example/post_review_int_service/PostReviewIntServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.post_review_int_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PostReviewIntServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<PostReviewIntServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/post_review_int_service/PostReviewIntServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.post_review_int_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PostReviewIntServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
